9ab2fe0008fc2f6d3264e4cc8438619d243622b1
wangyf <wangyf@bytedance.com>
2026-01-17 20:48:58 +1100

    v1.1

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/fivemeepo/mainrepo/lib"
 	"github.com/fivemeepo/mainrepo/subrepo"
 )
 
 func main() {
 	lib.Hello()
+	fmt.Println("main repo version: v1.1")
 	subrepo.Hello()
 }
