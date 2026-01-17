452981aa2243ef72408b7aff62057fb9ce225979
wangyf <wangyf@bytedance.com>
2026-01-17 14:22:26 +1100

    Update subrepo and mainrepo

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/fivemeepo/mainrepo/lib"
 	"github.com/fivemeepo/mainrepo/subrepo"
 )
 
 func main() {
 	lib.Hello()
-	subrepo.Hello()
+	fmt.Println(subrepo.Hello())
 }
